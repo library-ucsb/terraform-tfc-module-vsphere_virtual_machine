916623059d657fd128f5ad968691e9a3f4a01dd0
Josh <josh@joshpreston.net>
2022-03-29 19:36:06 -0700

    init

---
+++ main.tf
@@ -0,0 +1,79 @@
+
+data "vsphere_datacenter" "target_dc" {
+  name                            = var.deploy_vsphere_datacenter
+}
+
+data "vsphere_datastore" "target_datastore" {
+  name                            = var.deploy_vsphere_datastore
+  datacenter_id                   = data.vsphere_datacenter.target_dc.id
+}
+
+data "vsphere_compute_cluster" "target_cluster" {
+  name                            = var.deploy_vsphere_cluster
+  datacenter_id                   = data.vsphere_datacenter.target_dc.id
+}
+
+data "vsphere_network" "private_v2476" {
+  name                            = var.deploy_vsphere_network_v2476
+  datacenter_id                   = data.vsphere_datacenter.target_dc.id
+}
+
+data "vsphere_virtual_machine" "source_template" {
+  name                            = var.node_template
+  datacenter_id                   = data.vsphere_datacenter.target_dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name                            = var.node_host_name 
+  wait_for_guest_net_timeout      = var.wait_for_guest_net_timeout
+  resource_pool_id                = data.vsphere_compute_cluster.target_cluster.resource_pool_id
+  datastore_id                    = data.vsphere_datastore.target_datastore.id
+  tags                            = var.tags
+  folder                          = var.folder
+  num_cpus                        = var.node_vcpus
+  memory                          = var.node_memory
+  guest_id                        = data.vsphere_virtual_machine.source_template.guest_id
+
+  scsi_type                       = data.vsphere_virtual_machine.source_template.scsi_type
+
+  network_interface {
+    network_id                    = data.vsphere_network.private_v2476.id
+    adapter_type                  = "e1000"
+  }
+
+  disk {
+    label                         = "disk0"
+    size                          = var.node_disk_size
+    datastore_id                  = data.vsphere_datastore.target_datastore.id
+    unit_number                   = 0
+  }
+
+  clone {
+    template_uuid                 = data.vsphere_virtual_machine.source_template.id
+
+    customize {
+      linux_options {
+        host_name                 = var.node_host_name
+        domain                    = var.node_domain
+      }
+
+      network_interface {}
+
+      dns_server_list             = var.node_dns_servers
+      dns_suffix_list             = var.node_dns_suffix
+    }
+  }
+}
+
+
+output "vsphere_vm_hostname" {
+  value                           = vsphere_virtual_machine.vm.name
+}
+
+output "vsphere_vm_default_ip_confluence" {
+  value                           = vsphere_virtual_machine.vm.default_ip_address
+}
+
+output "vsphere_vm_guest_ips_confluence" {
+  value                           = vsphere_virtual_machine.vm.guest_ip_addresses
+}
---
+++ variables.tf
@@ -0,0 +1,70 @@
+variable "deploy_vsphere_datacenter" {}
+variable "deploy_vsphere_cluster" {}
+variable "deploy_vsphere_datastore" {}
+
+variable "deploy_vsphere_network_v2476" {
+  type        = string
+  default     = "dvPG-vlan2476"
+}
+
+variable "node_host_name" {}
+
+variable "node_wait_for_guest_net_timeout" {
+  type        = number
+  default     = 0
+}
+
+variable "node_template" {
+  type        = string
+  default     = "ucsb-lib-template-packer-core-linux-centos-7"
+}
+
+variable "node_vcpus" {
+  description = "The number of virtual processors to assign to this virtual machine. Default: 1."
+  default     = "1"
+}
+
+variable "node_memory" {
+  default     = "1024"
+}
+
+variable "node_dns_servers" {
+  type        = list
+  default     = [
+    "10.3.112.36",
+    "10.3.112.16"
+  ]
+}
+
+variable "node_dns_suffix" {
+  type        = list
+  default     = [
+    "library.ucsb.edu"
+  ]
+  description = "A list of DNS search domains to add to the DNS configuration on the virtual machine."
+}
+
+variable "node_domain" {
+  type        = string
+  description = "The domain name for this machine."
+  default     = "library.ucsb.edu"
+}
+
+variable "node_disk_size" {
+  description = "Size of the node root disk represented in GBs"
+  default     = "30"
+}
+
+variable "wait_for_guest_net_timeout" {
+  type        = number
+  default     = 0
+}
+
+variable "tags" {
+  type        = list
+  default     = []
+}
+
+variable "folder" {
+  type        = string
+}
